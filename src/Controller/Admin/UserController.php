79b5095dc37cea8e45f08f12e8d0da488b920208
dimitri vandevelde <vandevelde.dimitri@hotmail.com>
2024-08-19 15:16:02 +0200

    feature liste users

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/user')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'app_admin_user')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/user/index.html.twig', [
+            'controller_name' => 'UserController',
+            'users' => $users
+        ]);
+    }
+}
